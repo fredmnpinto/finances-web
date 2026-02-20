66b9a92bef01d86cead928a55ec5970d6893a6ef
Frederico Pinto <fredmnpinto@gmail.com>
2026-02-20 19:39:29 +0000

    Ran categories migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_17_100949) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_19_100000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.string "icon"
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id", "name"], name: "index_categories_on_user_id_and_name", unique: true
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "transactions", id: :serial, force: :cascade do |t|
     t.decimal "amount", null: false
     t.decimal "balance"
-    t.decimal "category_confidence"
-    t.text "category_source"
-    t.text "confirmed_category"
-    t.datetime "confirmed_category_at"
+    t.bigint "category_id"
     t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
     t.date "date", null: false
     t.text "description", null: false
     t.text "source_file"
-    t.text "suggested_category"
+    t.bigint "suggested_category_id"
     t.integer "transaction_type"
     t.bigint "user_id", null: false
     t.date "value_date"
+    t.index ["category_id"], name: "index_transactions_on_category_id"
+    t.index ["suggested_category_id"], name: "index_transactions_on_suggested_category_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
     t.unique_constraint ["date", "description", "amount", "balance"], name: "transactions_date_description_amount_balance_key"
   end
@@ -56,5 +66,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_17_100949) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "transactions", "categories"
+  add_foreign_key "transactions", "categories", column: "suggested_category_id"
   add_foreign_key "transactions", "users"
 end
