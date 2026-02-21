8119177dba327b5208a4896d1b88d9db36e16015
Frederico Pinto <fredmnpinto@gmail.com>
2026-02-21 23:40:35 +0000

    TAILWINDCSS: Tailwind FINALLY works with shell.nix again :relief:

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ pkgs.mkShell {
     pkgs.ruby_3_4
     pkgs.bundler
     pkgs.nodejs
+    pkgs.nodePackages.tailwindcss
     pkgs.yarn
     pkgs.postgresql
     pkgs.pkg-config
@@ -18,7 +19,8 @@ pkgs.mkShell {
     export GEM_HOME="$(pwd)/vendor/bundle/ruby/3.4.0"
     export PATH="$GEM_HOME/bin:$HOME/.local/share/gem/ruby/3.3.0/bin:$PATH"
     export RAILS_ENV=development
-    
+    export TAILWINDCSS_INSTALL_DIR=${pkgs.tailwindcss}/bin
+
     # Install gems if needed
     if [ ! -d "vendor/bundle" ]; then
       bundle config set --local path '"'"'vendor/bundle'"'"'
