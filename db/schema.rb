df114c8911c1dcf9eaa9c9c32d262d581bc35c5e
Frederico Pinto <fredmnpinto@gmail.com>
2026-02-22 11:42:41 +0000

    schema: category_group enum on categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_19_100000) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_19_100001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.integer "category_group", default: 0, null: false
     t.string "color"
     t.datetime "created_at", null: false
     t.string "icon"
