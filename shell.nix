5d56347f39b7b6b038e12f4f910936780eb343ec
Frederico Pinto <fredmnpinto@gmail.com>
2026-03-08 11:23:11 +0000

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -17,13 +17,13 @@ pkgs.mkShell {
 
   shellHook = ''
     export GEM_HOME="$(pwd)/vendor/bundle/ruby/3.4.0"
-    export PATH="$GEM_HOME/bin:$HOME/.local/share/gem/ruby/3.3.0/bin:$PATH"
+    export PATH="$GEM_HOME/bin:$HOME/.local/share/gem/ruby/3.4.0/bin:$PATH"
     export RAILS_ENV=development
     export TAILWINDCSS_INSTALL_DIR=${pkgs.tailwindcss}/bin
 
     # Install gems if needed
     if [ ! -d "vendor/bundle" ]; then
-      bundle config set --local path '"'"'vendor/bundle'"'"'
+      bundle config set --local path "vendor/bundle"
       bundle install
     fi
   '';
