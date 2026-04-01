3e485648a0d6271d976eaf1f2ccfa74f268306fe
Frederico Pinto <fredmnpinto@gmail.com>
2026-04-01 14:31:47 +0100

    Fix Sentry initialization and enable sync sending

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,8 @@ Sentry.init do |config|
   config.dsn = ENV.fetch("SENTRY_DSN", nil)
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
   config.send_default_pii = true
+  config.environment = Rails.env
+  config.enabled_environments = []
+  config.background_worker_threads = 0
+  config.debug = true
 end
